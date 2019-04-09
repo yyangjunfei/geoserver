bafd8ac91c6ad4138ceeb1fc43787e85bab68398
leocol <39551715+leocoel@users.noreply.github.com>
2019-04-09 09:18:11 +0800

    Update GetWorkController.java

--- java/com/leoco/getworkstore/controller/GetWorkController.java
+++ java/com/leoco/getworkstore/controller/GetWorkController.java
@@ -26,47 +26,42 @@ public class GetWorkController {
         String workname = request.getParameter("workname");
         String storename = request.getParameter("storename");
         String tablename = request.getParameter("tablename");
-        //GeoServer的连接配置
         String url = "http://localhost:8080/geoserver";
         String username = "admin";
         String passwd = "geoserver";
-        //Postgis连接配置
         String postgisHost = "localhost";
         int postgisPort = 5432;
         String postgisUser = "postgres";
-        String postgisPassword = "888888";
-        String postgisDatabase = "postgis_23_sample";
-        //判断工作区是否存在，不存在创建
+        String postgisPassword = "";
+        String postgisDatabase = "";
         URL u = new URL(url);
         GeoServerRESTManager manager = new GeoServerRESTManager(u, username, passwd);
         GeoServerRESTPublisher publisher = manager.getPublisher();
         List<String> workspaces = manager.getReader().getWorkspaceNames();
         if (!workspaces.contains(workname)) {
             boolean createws = publisher.createWorkspace(workname);
-            System.out.println("创建工作区成功! : " + createws);
+            System.out.println("创建成功! : " + createws);
         } else {
-            System.out.println("工作区已经存在! :" + workname);
+            System.out.println("已经存在! :" + workname);
         }
-        //判断数据存储是否存在，不存在创建
         RESTDataStore restStore = manager.getReader().getDatastore(workname, storename);
         if (restStore == null) {
             GSPostGISDatastoreEncoder store = new GSPostGISDatastoreEncoder(storename);
-            store.setHost(postgisHost);//url
-            store.setPort(postgisPort);//端口
-            store.setUser(postgisUser);// 数据库用户名
-            store.setPassword(postgisPassword);// 数据库密码
-            store.setDatabase(postgisDatabase);// 数据库类型
-            store.setSchema("public"); //公共public
-            store.setConnectionTimeout(20);// 超时设置
-            store.setMaxConnections(20); // 最大连接数
-            store.setMinConnections(1);   // 最小连接数
+            store.setHost(postgisHost);
+            store.setPort(postgisPort);
+            store.setUser(postgisUser);
+            store.setPassword(postgisPassword);
+            store.setDatabase(postgisDatabase);
+            store.setSchema("public"); 
+            store.setConnectionTimeout(20);
+            store.setMaxConnections(20);
+            store.setMinConnections(1);
             store.setExposePrimaryKeys(true);
             boolean createStore = manager.getStoreManager().create(workname, store);
-            System.out.println("创建数据存储成功! : " + createStore);
+            System.out.println("创建成功! : " + createStore);
         } else {
-            System.out.println("数据存储已经存在!:" + storename);
+            System.out.println("已经存在!:" + storename);
         }
-        //判断图层是否存在，不存在创建并发布
         RESTLayer layer = manager.getReader().getLayer(workname, tablename);
         if (layer == null) {
             GSFeatureTypeEncoder pds = new GSFeatureTypeEncoder();
